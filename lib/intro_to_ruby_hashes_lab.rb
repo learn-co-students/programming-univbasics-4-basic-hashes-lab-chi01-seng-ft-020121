1cf8bf4f1336d083cf2ebb03aab1383a9c533722
Alexandra Peccia <ampeccia@gmail.com>
2021-01-04 04:31:48 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,19 @@
 def new_hash
-  # return an empty hash
+  Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  fave_song = {
+    :artist => "Vulfpeck",
+    :song => "Dean Town"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer_hash = { :name => 'Grace Hopper' } 
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  hash_generator = { :id => number }
 end
